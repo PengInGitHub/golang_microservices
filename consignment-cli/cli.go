c1f85fe63ebe42413a62af74e8fe297cc2b1e32f
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-01 21:18:22 +0100

    finish cli.go

--- consignment-cli/cli.go
+++ consignment-cli/cli.go
@@ -7,4 +7,30 @@ import(
     "os"
 
     pb "github.com/user/golang_microservice/ewanvalentine/shipper/consignment-service/proto/consignment"
+    "golang/org/x/net/context"
+    "google.golang.org/grpc"
 )
+
+const(
+    address = "localhost:500501"
+    defaultFilename = "consignment.json"
+)
+
+func parseFile(file string)(*pb.Consignment, error){
+    var consignment *pb.Consignment
+    data, err := ioutil.ReadFile(file)
+    if err != nil{
+        return nil, err
+    }
+    json.Unmarshal(data,&consignment)
+    return consignment, err
+}
+
+func main{
+    //set up a connection to the server
+    conn, err := grpc.Dial(address, grpc.WithInsecure())
+    if err != nil{
+        log.Fatalf("Could not greet: %v ", err)
+    }
+    log.Printf("Created: %t", r.Created)
+}
