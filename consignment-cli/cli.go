87ebd121c99d3345250883af4c120f1e2502bb0b
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-01 20:51:32 +0100

    add cli.go

---
+++ consignment-cli/cli.go
@@ -0,0 +1,10 @@
+//consignment-cli/cli.go
+package main
+import(
+    "encoding/json"
+    "io/ioutil"
+    "log"
+    "os"
+
+    pb "github.com/user/golang_microservice/ewanvalentine/shipper/consignment-service/proto/consignment"
+)
