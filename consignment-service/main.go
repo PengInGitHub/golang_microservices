bad78c6f47bcb203febdb2fe2c24d00eaea4282c
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-01 22:44:53 +0100

    finish main.go

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -31,3 +31,66 @@ func (repo *Repository) Create(consignment *pb.Consignment)(*pb.Consignment, err
     return consignment, nil 
 }
 
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc 
+// to give you a better idea. 
+
+type service struct{
+    repo IRepositry
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an 
+// argument, these are handled by the gRPC server. 
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment)(*pb.Response, error){
+    //save our consignment
+    consignment, err := s.repo.Create(req)
+    if err != nil {
+        return nil, err
+    }
+    //Return matching the 'Response' message we created in our protobuf definition
+    return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func main(){
+    repo := Repositry{}
+    
+    //set-up our grpc server
+    lis, err := net.Listen("tcp", port)
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+    s := grpc.NewServer()
+    // Register our service with the gRPC server, this will tie our
+    // implementation into the auto-generated interface code for our
+    // protobuf definition. 
+    pb.RegisterShippingServiceServer(s, &service{repo})
+    
+    //Register reflection service on gRPC server
+    reflection.Register(s)
+    if err := s.Serve(lis); err != nil {
+        log.Fatalf("failed to serve: %v", err)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
